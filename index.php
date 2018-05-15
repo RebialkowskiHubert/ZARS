4dd64ea595b607634d0dba73e6234f3e7aee468c
Hubert Rebialkowski <hubertrebialkowski@gmail.com>
2018-05-15 11:57:54 +0200

    Zapisz

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 session_start();
 if(isset($_SESSION["login"]))
 	header('Location: view/uzytkownik.php');
 else
 	header('Location: view/glowna.php');
-=======
-<?php
-session_start();
-if(isset($_SESSION["login"]))
-	header('Location: view/uzytkownik.php');
-else
-	header('Location: view/glowna.php');
->>>>>>> 6f4311abe3f2332758f3a04f92fa97e9ac5af4d6
 ?>
